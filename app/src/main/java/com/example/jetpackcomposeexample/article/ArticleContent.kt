34a8eb6c7a9eb94602da4178a546faaf64d263c2
Nguyễn Trần Anh Khôi <92618299+anhkhoi1511619@users.noreply.github.com>
2023-10-22 22:01:31 +0900

    Initial Code

---
+++ app/src/main/java/com/example/jetpackcomposeexample/article/ArticleContent.kt
@@ -0,0 +1,191 @@
+package com.example.jetpackcomposeexample.article
+
+import android.text.BoringLayout
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposeexample.R
+import com.example.jetpackcomposeexample.data.Post
+import com.example.jetpackcomposeexample.data.impl.post3
+import com.example.jetpackcomposeexample.ui.theme.JetpackComposeExampleTheme
+import com.example.jetpackcomposeexample.utils.BookmarkButton
+import com.example.jetpackcomposeexample.utils.FavoriteButton
+import com.example.jetpackcomposeexample.utils.ShareButton
+import com.example.jetpackcomposeexample.utils.TextSettingButton
+
+@ExperimentalMaterial3Api
+@Composable
+fun ArticleScreen(
+    post: Post,
+    isExpandedScreen: Boolean,
+    onBack: () -> Unit,
+    isFavorite: Boolean,
+    onToggleFavorite: () -> Unit,
+    modifier: Modifier = Modifier,
+    lazyListState: LazyListState = rememberLazyListState()
+) {
+    var showUnimplementedActionDialog by rememberSaveable { mutableStateOf(false) }
+    if (showUnimplementedActionDialog) {
+        FunctionalityNotAvailablePopup { showUnimplementedActionDialog = false }
+    }
+    Row(Modifier.fillMaxSize()) {
+        val context = LocalContext.current
+        ArticleScreenContent(
+            post = post,
+            navigationIconContent = {
+                if (!isExpandedScreen) {
+                    IconButton(onClick = onBack) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.cd_navigate_up),
+                            tint = MaterialTheme.colorScheme.primary
+                            )
+
+                    }
+                }
+            },
+            bottomBarContent = {
+                if (!isExpandedScreen) {
+                    BottomAppBar (
+                        actions = {
+                            FavoriteButton (onClick = {showUnimplementedActionDialog = true})
+                            BookmarkButton(isBookmarked = isFavorite, onClick = onToggleFavorite)
+                            ShareButton {}
+                            TextSettingButton (onClick = {showUnimplementedActionDialog = true})
+                        }
+                    )
+                }
+            },
+            lazyListState = lazyListState
+            )
+    }
+}
+
+
+@ExperimentalMaterial3Api
+@Composable
+private fun ArticleScreenContent(
+    post: Post,
+    navigationIconContent: @Composable ()->Unit = { },
+    bottomBarContent: @Composable ()->Unit = { },
+    lazyListState: LazyListState = rememberLazyListState()
+) {
+    val topAppBarState = rememberTopAppBarState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(topAppBarState)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = post.publication?.name.orEmpty(),
+                navigationIconContent = navigationIconContent,
+                scrollBehavior = scrollBehavior,
+            )
+        },
+        bottomBar = bottomBarContent
+    ) {innerPadding->
+        PostContent(post = post,
+            modifier = Modifier
+                .nestedScroll(scrollBehavior.nestedScrollConnection)
+                .padding(innerPadding),
+        state = lazyListState
+    )
+    }
+}
+
+@ExperimentalMaterial3Api
+@Composable
+private fun TopAppBar (
+    title: String,
+    navigationIconContent: @Composable () -> Unit,
+    scrollBehavior: TopAppBarScrollBehavior?,
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(title = {
+        Row {
+            Image(painter = painterResource(id = R.drawable.icon_article_background),
+                contentDescription = null,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(36.dp))
+            Text(
+                text = stringResource(id = R.string.published_in, title),
+                style = MaterialTheme.typography.labelLarge,
+                modifier = Modifier.padding(8.dp)
+                )
+        }
+    },
+        navigationIcon = navigationIconContent,
+        scrollBehavior = scrollBehavior,
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun FunctionalityNotAvailablePopup(onDismiss: () -> Unit) {
+    AlertDialog(onDismissRequest = onDismiss,
+        text = {
+            Text(text = stringResource(id = R.string.article_functionality_not_available),
+                style = MaterialTheme.typography.bodyMedium)
+        },
+        confirmButton = {
+            TextButton(onClick = onDismiss) {
+                Text(text = stringResource(id = R.string.close))
+            }
+        })
+}
+
+@ExperimentalMaterial3Api
+@Preview
+@Composable
+fun PreviewArticleContent() {
+    JetpackComposeExampleTheme {
+        ArticleScreenContent(post = post3,{},{})
+    }
+}
+
+@ExperimentalMaterial3Api
+@Preview
+@Composable
+fun PreviewArticleScreen() {
+    JetpackComposeExampleTheme {
+        ArticleScreen(
+            post = post3,
+            isExpandedScreen = false,
+            onBack = { /*TODO*/ },
+            isFavorite = false,
+            onToggleFavorite = { /*TODO*/ })
+    }
+}
---
+++ app/src/main/java/com/example/jetpackcomposeexample/data/impl/PostData.kt
@@ -0,0 +1,120 @@
+package com.example.jetpackcomposeexample.data.impl
+
+import com.example.jetpackcomposeexample.R
+import com.example.jetpackcomposeexample.data.Markup
+import com.example.jetpackcomposeexample.data.MarkupType
+import com.example.jetpackcomposeexample.data.MetaData
+import com.example.jetpackcomposeexample.data.Paragraph
+import com.example.jetpackcomposeexample.data.ParagraphType
+import com.example.jetpackcomposeexample.data.Post
+import com.example.jetpackcomposeexample.data.PostAuthor
+import com.example.jetpackcomposeexample.data.Publication
+
+val publication = Publication(
+    "Android Developers",
+    "https://cdn-images-1.medium.com/max/258/1*u7oZc2_5mrkcFaxkXEyfYA@2x.png"
+)
+val florina = PostAuthor(
+    "Florina Muntenescu",
+    "https://medium.com/@florina.muntenescu"
+)
+
+val paragraphsPost3 = listOf(
+    Paragraph(
+        ParagraphType.Text,
+        "Learn how to get started converting Java Programming Language code to Kotlin, making it more idiomatic and avoid common pitfalls, by following our new Refactoring to Kotlin codelab, available in English \uD83C\uDDEC\uD83C\uDDE7, Chinese \uD83C\uDDE8\uD83C\uDDF3 and Brazilian Portuguese \uD83C\uDDE7\uD83C\uDDF7.",
+        listOf(
+            Markup(
+                MarkupType.Link,
+                151,
+                172,
+                "https://codelabs.developers.google.com/codelabs/java-to-kotlin/#0"
+            ),
+            Markup(
+                MarkupType.Link,
+                209,
+                216,
+                "https://clmirror.storage.googleapis.com/codelabs/java-to-kotlin-zh/index.html#0"
+            ),
+            Markup(
+                MarkupType.Link,
+                226,
+                246,
+                "https://codelabs.developers.google.com/codelabs/java-to-kotlin-pt-br/#0"
+            )
+        )
+    ),
+    Paragraph(
+        ParagraphType.Text,
+        "When you first get started writing Kotlin code, you tend to follow Java Programming Language idioms. The automatic converter, part of both Android Studio and Intellij IDEA, can do a pretty good job of automatically refactoring your code, but sometimes, it needs a little help. This is where our new Refactoring to Kotlin codelab comes in.",
+        listOf(
+            Markup(
+                MarkupType.Link,
+                105,
+                124,
+                "https://www.jetbrains.com/help/idea/converting-a-java-file-to-kotlin-file.html"
+            )
+        )
+    ),
+    Paragraph(
+        ParagraphType.Text,
+        "We’ll take two classes (a User and a Repository) in Java Programming Language and convert them to Kotlin, check out what the automatic converter did and why. Then we go to the next level — make it idiomatic, teaching best practices and useful tips along the way.",
+        listOf(
+            Markup(MarkupType.Code, 26, 30),
+            Markup(MarkupType.Code, 37, 47)
+        )
+    ),
+    Paragraph(
+        ParagraphType.Text,
+        "The Refactoring to Kotlin codelab starts with basic topics — understand how nullability is declared in Kotlin, what types of equality are defined or how to best handle classes whose role is just to hold data. We then continue with how to handle static fields and functions in Kotlin and how to apply the Singleton pattern, with the help of one handy keyword: object. We’ll see how Kotlin helps us model our classes better, how it differentiates between a property of a class and an action the class can do. Finally, we’ll learn how to execute code only in the context of a specific object with the scope functions.",
+        listOf(
+            Markup(MarkupType.Code, 245, 251),
+            Markup(MarkupType.Code, 359, 365),
+            Markup(
+                MarkupType.Link,
+                4,
+                25,
+                "https://codelabs.developers.google.com/codelabs/java-to-kotlin/#0"
+            )
+        )
+    ),
+    Paragraph(
+        ParagraphType.Text,
+        "Thanks to Walmyr Carvalho and Nelson Glauber for translating the codelab in Brazilian Portuguese!",
+        listOf(
+            Markup(
+                MarkupType.Link,
+                21,
+                42,
+                "https://codelabs.developers.google.com/codelabs/java-to-kotlin/#0"
+            )
+        )
+    ),
+    Paragraph(
+        ParagraphType.Text,
+        "",
+        listOf(
+            Markup(
+                MarkupType.Link,
+                76,
+                96,
+                "https://codelabs.developers.google.com/codelabs/java-to-kotlin-pt-br/#0"
+            )
+        )
+    )
+)
+val post3 = Post(
+    id = "ac552dcc1741",
+    title = "From Java Programming Language to Kotlin — the idiomatic way",
+    subtitle = "Learn how to get started converting Java Programming Language code to Kotlin, making it more idiomatic and avoid common pitfalls, by…",
+    url = "https://medium.com/androiddevelopers/from-java-programming-language-to-kotlin-the-idiomatic-way-ac552dcc1741",
+    publication = publication,
+    metaData = MetaData(
+        author = florina,
+        date = "July 09",
+        readTimeMinutes = 1
+    ),
+    paragraph = paragraphsPost3,
+    imageId = R.drawable.post_3,
+    imageThumbId = R.drawable.post_3_thumb
+)
