8cf201b7f4a8910d09d18c8e1af197a927ca8110
Nguyễn Trần Anh Khôi <92618299+anhkhoi1511619@users.noreply.github.com>
2024-02-03 13:25:31 +0900

    Add permission class

---
+++ app/src/main/java/com/example/jetpackcomposeexample/controller/PermissionController.java
@@ -0,0 +1,6 @@
+package com.example.jetpackcomposeexample.controller;
+
+public class PermissionController {
+    //TODO: Check WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE, MANAGE_EXTERNAL_STORAGE
+    //Goal: Permission for writing content to send to server
+}
